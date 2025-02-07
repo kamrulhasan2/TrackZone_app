87ea0851ec93f2f1cd4a247e42a46a784747564b
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-02-07 19:48:36 +0600

    create an object of Timezone offset

--- src/hooks/useClock.js
+++ src/hooks/useClock.js
@@ -13,6 +13,15 @@ const init = {
     date: null
 };
 
+const TIMEZONE_OFFSET = {
+    PST: -7 * 60,
+    EST: -4 * 60,
+    EDT: -4 * 60,
+    BST: 1 * 60,
+    MST: -6 * 60
+};
+
+
 const useClock = () => {
     const [state,setState] = useState({...init});
     const [utc, setUtc] = useState(null);
