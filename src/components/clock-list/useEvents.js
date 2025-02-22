b055a7773334da1c5b51a0251c9bb0b073f40d5a
kamrulhasan2 <kamrulhasan20656@gmail.com>
2025-02-22 19:58:15 +0600

    create a function getEventsByClockId

--- src/components/clock-list/useEvents.js
+++ src/components/clock-list/useEvents.js
@@ -2,8 +2,15 @@ import React, { useState } from 'react'
 
 const useEvents = () => {
     const [state,setState] = useState();
+
+    const getEventsByClockId = (clockId) =>{
+        return Object.keys(state).filter((item) => item.startsWith(clockId));
+    }
+
+
   return {
     events: state,
+    getEventsByClockId,
   }
 }
 
